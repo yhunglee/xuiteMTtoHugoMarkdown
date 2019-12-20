501f8a7c5ea88dad8f51a89997a6549838c358a7
HowardLee <yhunglee@gmail.com>
2019-12-21 00:21:20 +0800

    feat(transformer.rb): implement of mapping data and generate files.
    Implement functions of mapping meta and content from xuite file to hugo
    format. Also implement function of generating markdown files of articles
    for hugo

--- transformer.rb
+++ transformer.rb
@@ -4,13 +4,18 @@
 # Transform exported M.T. file(Movable type file) of xuite blog to be markdown-type file of hugo static site generator with liva-hugo theme posts.
 
 require 'reverse_markdown'
+require 'date'
+require 'yaml'
 
 # load input file
-input_file = File.open('./input/sample-input.txt_', "r")
+# Notice: need to save input file as LF as line separator. If it is CRLF, convert it to LF.
+input_file = File.open('./input/xuite_blog_export_2008876.txt_', "r")
+#input_file = File.open('./input/sample-input.txt_', "r")
 raw_content = input_file.read
 input_file.close
+
+# need append file first 8 bytes with "-" character
 raw_content.prepend("--------\n")
-puts raw_content #debug
 
 
 # split meta and body
@@ -18,25 +23,22 @@ meta = Array.new
 body = Array.new
 
 
-# need append file first 8 bytes with "-" character
 
+# extract meta from raw file content
 meta_match = raw_content.gsub(/(?<=\-{8}).*?(?=-{4})/um)
-# puts "meta_match[]: " + meta_match.next
-# puts "1==="
-# puts "meta_match[]: " + meta_match.next
-# puts "2==="
-# puts "meta_match[]: " + meta_match.next
-# puts "3==="
+# meta_match.with_index { |content, idx|
+#     puts "index #{idx} is #{content}" #debug
+# }
 
 
 
+# extract body of post from raw file content
 body_match = raw_content.gsub(/(?<=BODY:\s).*?(?=\-{5})/um)
-# puts "body_match[]: " + body_match.next
-# puts "1==="
-# puts "body_match[]: " + body_match.next
-# puts "2==="
-# puts "body_match[]: " + body_match.next
-# puts "3==="
+# body_match.with_index { |a,index|
+#     puts "index #{index} is #{a}" #debug
+# }
+
+
 
 
 # mapping meta to hugo post
@@ -70,6 +72,44 @@ publish/draft          true/false
 
 =end
 
+tmpHash = Hash.new
+
+meta_match.with_index { |meta, idx| 
+
+    # create a new hash for each post
+    tmpHash[idx] = Hash.new
+
+    # TITLE to title
+    tmpHash[idx]["title"] = /(?<=TITLE:\s).+/u.match(meta).to_s
+
+    # DATE to date
+    tmpHash[idx]['date'] = DateTime.strptime(/(?<=DATE:\s).+/u.match(meta).to_s.concat(' +08'), '%m/%d/%Y %I:%M:%S %p %z').to_s
+
+    # CATEGORY to categories
+    tmpHash[idx]['categories'] = [/(?<=CATEGORY:\s).+/u.match(meta).to_s]
+
+    # gen image meta
+    tmpHash[idx]['image'] = ''
+
+    # gen description meta
+    tmpHash[idx]['description'] = tmpHash['title']
+
+    # gen draft meta
+    tmpDraft = /(?<=STATUS:\s).+/u.match(meta).to_s
+    tmpHash[idx]['draft'] = (tmpDraft.casecmp('publish') === 0)? false : true
+
+    # gen tags meta
+    tmpHash[idx]['tags'] = []
+
+    # gen type meta
+    tmpHash[idx]['type'] = 'post'
+
+    # gen highlight meta
+    tmpHash[idx]['highlight'] = false
+
+
+}
+
 =begin
     body transformation
 
@@ -80,10 +120,21 @@ publish/draft          true/false
 
 # tranform body to markdown content
 # use reverse_markdown gem
+tmpContent = Hash.new
+body_match.with_index {|content, idx|
 
+    tmpContent[idx] = ReverseMarkdown.convert content
+
+}
 
 
 
 # output every post a separated file
 # output file name is test-timestamp.md
-
+# Notice: f.puts \-{3} and two newline
+meta_match.with_index { |_, index|
+    File.open("./output/#{index}.md", "w") { |f|
+        f.write(tmpHash[index].to_yaml)
+        f.puts("---", "", "", tmpContent[index])
+    }
+}
